3f116a74a451674ead95b668ddbefd1d6590f590
Juan Jose Ramirez <ramirez.orellana.juanjose@gmail.com>
2020-10-11 20:51:50 -0600

    Add Product Detail template

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    # print(args, kwargs)
-    # print(request.user)
-    return render(request, 'home.html', {})
+    print(args, kwargs)
+    print(request.user)
+    my_result = {
+        'my_text': 'Testing',
+        'my_number': 123456,
+        'my_list': [123, 3223, 23123]
+    }
+    return render(request, 'home.html', my_result)
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from pages.views import home_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 
